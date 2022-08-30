131accde6579be273518d3fd19acbb5fc4c32c33
dmshch <dmshch>
2022-08-30 13:47:40 +0300

    script for init DB

--- init_db.py
+++ init_db.py
@@ -4,36 +4,42 @@ from web import database
 import sqlalchemy as sa
 import bcrypt
 
-def init():
+def first_init_db():
     with open('schema.sql', 'r', encoding='utf-8') as schema:
         t = sa.text(schema.read())
-    '''
-    if bcrypt.checkpw(password, hashed):
-        print("It Matches!")
-    else:
-        print("It Does not Match :(")
-    '''
+    admin_1, admin_2, monitor_1, monitor_2 = "" , "", "", ""
+    while admin_1 == "" or admin_2 == "" or admin_1 != admin_2:
+        admin_1 = input("Please type password for user admin:")
+        admin_2 = input("Please retype password for user admin:")
+        if admin_1 != admin_2:
+            print("The entered passwords do not match. Please retype.")
+    while monitor_1 == "" or monitor_2 == "" or monitor_1 != monitor_2:
+        monitor_1 = input("Please type password for user monitor:")
+        monitor_2 = input("Please retype password for user monitor:")
+        if monitor_1 != monitor_2:
+            print("The entered passwords do not match. Please retype.")
     with database.engine.connect() as conn:
         conn.execute(t)
         metadata = sa.MetaData()
         # for admin
-        if user == "admin":
-            password = bytes("admin".encode("utf-8"))
-            hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-            users = sa.Table('users', metadata, autoload=True, autoload_with=conn)
-            query = sa.update(users).values(password = hashed.decode("utf-8"))
-            query = query.where(users.columns.login == "admin")
-            results = conn.execute(query)
+        password = bytes(admin_2.encode("utf-8"))
+        hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+        users = sa.Table('users', metadata, autoload=True, autoload_with=conn)
+        query = sa.update(users).values(password = hashed.decode("utf-8"))
+        query = query.where(users.columns.login == "admin")
+        results = conn.execute(query)
         # for monitor
-        if user == "monitor":
-		    password = bytes("monitor".encode("utf-8"))
-            hashed = bcrypt.hashpw(password, bcrypt.gensalt())
-            users = sa.Table('users', metadata, autoload=True, autoload_with=conn)
-            query = sa.update(users).values(password = hashed.decode("utf-8"))
-            query = query.where(users.columns.login == "monitor")
-            results = conn.execute(query)
+        password = bytes(monitor_2.encode("utf-8"))
+        hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+        users = sa.Table('users', metadata, autoload=True, autoload_with=conn)
+        query = sa.update(users).values(password = hashed.decode("utf-8"))
+        query = query.where(users.columns.login == "monitor")
+        results = conn.execute(query)
+        # END
         conn.close()
         database.engine.dispose()
-		
+        
 if __name__ == "__main__":
-    init()
+    answer = input("Create new tables [type 'y' for create and 'q' for exit]? : ")
+    if answer == "y":
+        first_init_db()
